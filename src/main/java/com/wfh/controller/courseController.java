cb1dbe002277969001a82db92bfcd4c98ea83046
hl112 <forest.tl112@gmail.com>
2021-07-29 21:59:00 +0700

    feat(Course): Done

--- src/main/java/com/wfh/controller/courseController.java
+++ src/main/java/com/wfh/controller/courseController.java
@@ -1,12 +1,8 @@
 package com.wfh.controller;
 
-import java.util.List;
-
-import javax.validation.Valid;
-
-import com.wfh.dao.impl.UserInfoDAOImpl;
-import com.wfh.model.User;
-import com.wfh.model.UserInfo;
+import com.wfh.model.Course;
+import com.wfh.model.CourseCategory;
+import com.wfh.service.courseServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,9 +10,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.wfh.model.Course;
-import com.wfh.model.CourseCategory;
-import com.wfh.service.courseServices;
+import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequestMapping("/course")
