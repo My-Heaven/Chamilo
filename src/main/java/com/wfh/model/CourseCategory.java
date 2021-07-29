dcfdca6ae1449b91d60510183a4b67a7125b3adb
hl112 <forest.tl112@gmail.com>
2021-07-29 08:48:15 +0700

    fix(DB): Edit Table name CourseCategory -> course_category (InCode)

--- src/main/java/com/wfh/model/CourseCategory.java
+++ src/main/java/com/wfh/model/CourseCategory.java
@@ -6,7 +6,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name=" CourseCategory")
+@Table(name="course_category")
 public class CourseCategory {
 	@Id
 	@Column(name="id")
